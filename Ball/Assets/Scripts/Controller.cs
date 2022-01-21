3024001ca0bbac0d1e1e7b59ca7b2cdfc876bcf0
Edwin <edwinsantiagocastrobuitrago@gmail.com>
2022-01-20 20:53:39 -0500

    v.2

--- Ball/Assets/Scripts/Controller.cs
+++ Ball/Assets/Scripts/Controller.cs
@@ -1,44 +1,52 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
     //Variables
-    public float vAngle=0.0f;
-    public float maxAngle=0f;
-    public float centre =0f;
+    public float maxAngle = 0f;
+    public float centre = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
+    
     // Update is called once per frame
     void Update()
     {
         rotation(rotacionPerDistance());
     }
-    private float checkCoordsBall(){
-    float ballPosition=GameObject.FindGameObjectWithTag("Player").transform.position.x;
-    return ballPosition; 
+    private float checkCoordsBall()
+    {
+        float ballPosition = GameObject.FindGameObjectWithTag("Player").transform.position.x;
+        return ballPosition;
     }
     //angle valores desde -45 a 45
-    private void rotation(float angle){
-        gameObject.transform.rotation=Quaternion.Euler(new Vector3(0,0,angle));
+    private void rotation(float angle)
+    {
+        gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
-    private float rotacionPerDistance(){
-        float xBall=checkCoordsBall();
-        float a = (gameObject.transform.localScale.x/2)+centre;
-        float b = gameObject.transform.localScale.x-a;
-        if(xBall!=centre){
-            if(xBall<centre){
-                return (xBall*maxAngle)/a;
-            }else{
-                return (xBall*maxAngle)/b;
+    private float rotacionPerDistance()
+    {
+        float xBall = checkCoordsBall();
+        float a = (gameObject.transform.localScale.x / 2) + centre;
+        float b = gameObject.transform.localScale.x - a;
+        if (xBall != centre)
+        {
+            if (xBall < centre)
+            {
+                return (xBall * maxAngle) / a;
+            }
+            else
+            {
+                return (xBall * maxAngle) / b;
             }
 
-        }else{
+        }
+        else
+        {
             return 0;
         }
     }
